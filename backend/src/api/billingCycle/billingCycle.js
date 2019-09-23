c9c3b0218909a970c26dbdbb631f9835ca411a51
Domenico <domedfd@gmail.com>
2019-09-23 17:23:05 -0400

    Criado rota, e personalizado mensagens

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -2,13 +2,17 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  name: { type: String, required: true },
   value: { type: Number, min: 0, require: true }
 });
 
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Informe o valor do debito!"]
+  },
   status: {
     type: String,
     required: false,
@@ -18,11 +22,11 @@ const debtSchema = new mongoose.Schema({
 });
 
 const billingCycleSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  name: { type: String, required: true },
   month: { type: Number, min: 1, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 20100, required: true },
+  year: { type: Number, min: 1970, max: 2100, required: true },
   credits: [creditSchema],
-  debt: [debtSchema]
+  debts: [debtSchema]
 });
 
 module.exports = restful.model("BillingCycle", billingCycleSchema);
--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -6,6 +6,6 @@ module.exports = function(server) {
   server.use("/api", router);
 
   //Rotas de Ciclo de Pagamento
-  const BillingCycle = require("../api/billingCycle");
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
   BillingCycle.register(router, "/billingCycles");
 };
