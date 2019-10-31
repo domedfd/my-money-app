94a6b1172f28e0c8249a5c9fbcb2157ca908dfc9
Domenico <domedfd@gmail.com>
2019-10-31 14:41:59 -0300

    Creacion del menu, menu tree y menu item

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,3 +1,16 @@
 import React from "react";
+import MenuItem from "./menuItem";
+import MenuTree from "./menuTree";
 
-export default props => <ul className="sidebar-menu"></ul>;
+export default props => (
+  <ul className="sidebar-menu">
+    <MenuItem path="#/" label="Desahboard" icon="dashboard" />
+    <MenuTree label="Cadastro" icon="edit">
+      <MenuItem
+        path="#billingCycles"
+        label="Cliclos de Pagamentos"
+        icon="usd"
+      />
+    </MenuTree>
+  </ul>
+);
