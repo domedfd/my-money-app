66bf15563052b3a3c0a9dba0fdc8f04b321b586e
Domenico <domedfd@gmail.com>
2019-10-31 12:56:23 -0300

    Com problema no app.css

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     alias: {
       modules: __dirname + "/node_modules",
       jquery: "modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js",
-      bootstrap: "modules/admi-lte/bootstrap/js/bootstrap.js"
+      bootstrap: "modules/admin-lte/bootstrap/js/bootstrap.js"
     }
   },
   plugins: [
