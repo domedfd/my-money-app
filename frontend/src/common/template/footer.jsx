f84a7139798faf86007154d49296d447ed81c106
Domenico <domedfd@gmail.com>
2019-10-31 14:55:52 -0300

    creado footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2019
+      <a href="#/" target="_blank">
+        {" "}
+        Domgroup
+      </a>
+      .
+    </strong>
+  </footer>
+);
