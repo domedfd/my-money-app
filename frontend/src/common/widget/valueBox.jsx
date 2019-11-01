4bce11d4fdc502ae7131c060b30cec3d91a83143
Domenico <domedfd@gmail.com>
2019-11-01 15:02:44 -0300

    Crecion de grid, valuebox y row

--- frontend/src/common/widget/valueBox.jsx
+++ frontend/src/common/widget/valueBox.jsx
@@ -9,7 +9,7 @@ export default props => (
         <p>{props.text}</p>
       </div>
       <div className="icon">
-        <i className="fa fa-${props.icon}"></i>
+        <i className={`fa fa-${props.icon}`}></i>
       </div>
     </div>
   </Grid>
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,13 +2,39 @@ import React, { Component } from "react";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Dashboard" small="Version 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="Gs 10.000"
+              text="Total de Creditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="Gs 10.000"
+              text="Total de Debitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="Gs 0"
+              text="Valor consolidado"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
