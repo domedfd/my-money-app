d4aae075d28ebaca01218eda175c452cbd64c95b
Domenico <domedfd@gmail.com>
2019-11-04 18:51:56 -0300

    cirando action y reducer getList

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/tabReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer, //() => ({ summary: { credit: 100, debt: 50 } })
-  tab: TabReducer
+  tab: TabReducer,
+  billingCycle: BillingCycleReducer
 });
 export default rootReducer;
