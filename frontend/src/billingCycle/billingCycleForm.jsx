12291d85183c02adbafac151aa781fc61619218f
Domenico <domedfd@gmail.com>
2019-11-04 23:26:24 -0300

    Funcao botao cancelar

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import { reduxForm, Field } from "redux-form";
+
+import { init } from "./billingCycleActions";
 import labelAndInput from "../common/form/labelAndInput";
 
 class BillinCycleForm extends Component {
@@ -37,12 +41,26 @@ class BillinCycleForm extends Component {
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this.props.init}
+          >
+            Cancelar
+          </button>
         </div>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: "billingCycleForm", destroyOnUnmount: false })(
-  BillinCycleForm
-);
+BillinCycleForm = reduxForm({
+  form: "billingCycleForm",
+  destroyOnUnmount: false
+})(BillinCycleForm);
+
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
+export default connect(
+  null,
+  mapDispatchToProps
+)(BillinCycleForm);
