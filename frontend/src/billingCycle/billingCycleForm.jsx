415a23ad59efccda796d4a461ac54b402636a747
Domenico <domedfd@gmail.com>
2019-11-04 21:27:36 -0300

    Fazer o botao criar e colocando componente msg com redux

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
 class BillinCycleForm extends Component {
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form role="form">
-        <div className="box-body"></div>
+      <form role="form" onSubmit={handleSubmit}>
+        <div className="box-body">
+          <Field name="name" component="input" />
+          <Field name="month" component="input" />
+          <Field name="year" component="input" />
+        </div>
         <div className="box-footer">
           <button type="submit" className="btn btn-primary">
             Submit
@@ -15,4 +21,4 @@ class BillinCycleForm extends Component {
   }
 }
 
-export default BillinCycleForm;
+export default reduxForm({ form: "billingCycleForm" })(BillinCycleForm);
