6d896c6c86165449761b15f231091e1204d37cde
Domenico <domedfd@gmail.com>
2019-11-06 15:44:41 -0300

    rotas com token e  sem token

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const auth = require("./auth");
 
 module.exports = function(server) {
+  /*
   // defirnir URL base para todoas a rotas
   const router = express.Router();
   server.use("/api", router);
@@ -8,4 +10,35 @@ module.exports = function(server) {
   //Rotas de Ciclo de Pagamento
   const BillingCycle = require("../api/billingCycle/billingCycleService");
   BillingCycle.register(router, "/billingCycles");
+  */
+
+  //nuevo con auth
+
+  /**
+  |--------------------------------------------------
+  | Rotas Protegidas por token
+  |--------------------------------------------------
+  */
+
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
+
+  protectedApi.use(auth);
+
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  /**
+  |--------------------------------------------------
+  | Rotas abertas
+  |--------------------------------------------------
+  */
+
+  const openApi = express.Router();
+  server.use("/opai", openApi);
+
+  const AuthService = require("../api/user/authService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
 };
